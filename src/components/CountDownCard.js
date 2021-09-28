8ca05e8e4367c9de0691b0a34bada73c030336a4
dacitto <dacidigital@gmail.com>
2021-09-28 20:18:28 +0100

    init

---
+++ src/components/CountDownCard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CountDownCard() {
+  return <div className="card">00</div>;
+}
+
+export default CountDownCard;
---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Header() {
+  return <h1>We're launching soon Days Hours Minutes Seconds</h1>;
+}
+
+export default Header;
