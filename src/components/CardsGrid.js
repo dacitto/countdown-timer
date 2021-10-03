ffdb5ab5d61fed5658d0d45a5e0be045ea71f690
dacitto <dacidigital@gmail.com>
2021-10-03 23:11:54 +0100

    add react-countdown and configure it

--- src/components/CardsGrid.js
+++ src/components/CardsGrid.js
@@ -1,13 +1,30 @@
 import React from "react";
 import CountDownCard from "./CountDownCard";
+import Countdown from "react-countdown";
 
+// Random component
+const Completionist = () => <span>You are good to go!</span>;
+
+// Renderer callback with condition
+const renderer = ({ days, hours, minutes, seconds, completed }) => {
+  if (completed) {
+    return <h2>Welcome to our website</h2>;
+  } else {
+    // Render a countdown
+    return (
+      <>
+        <CountDownCard label="days" time={days}></CountDownCard>
+        <CountDownCard label="hours" time={hours}></CountDownCard>
+        <CountDownCard label="minuts" time={minutes}></CountDownCard>
+        <CountDownCard label="seconds" time={seconds++}></CountDownCard>
+      </>
+    );
+  }
+};
 const CardsGrid = () => {
   return (
     <div className="container">
-      <CountDownCard label="days"></CountDownCard>
-      <CountDownCard label="hours"></CountDownCard>
-      <CountDownCard label="minuts"></CountDownCard>
-      <CountDownCard label="seconds"></CountDownCard>
+      <Countdown date={Date.now() + 8640000000} renderer={renderer} />
     </div>
   );
 };
--- src/components/CountDownCard.js
+++ src/components/CountDownCard.js
@@ -1,14 +1,13 @@
 import React from "react";
-
-function CountDownCard({ label }) {
+function CountDownCard({ label, time }) {
   return (
     <div className="CountDownCard">
       <div className="card">
         <div className="card-part top">
-          <span className="number">85</span>
+          <span className="number">{time}</span>
         </div>
         <div className="card-part bottom">
-          <span className="number">85</span>
+          <span className="number">{time}</span>
         </div>
       </div>
       <h2 className="label">{label}</h2>
